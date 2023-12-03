cb12f1f877ced39b81dfa83481cf8a8b92927924
oneilh <obidiasodumebi@mail.com>
2023-12-03 17:35:17 +0100

    added db file and home component structure basics set

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import "./App.css";
+import Home from "./pages/home";
 
 function App() {
-  return(
-    <div className="">
-      <Home/>
+  return (
+    <div className="container">
+      <Home />
     </div>
-  )
-  }
+  );
+}
 
-export default App
+export default App;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,16 @@
+import Article from "../components/Article/Article";
+import styles from "./home.module.css";
 const Home = () => {
-    return ( <div className="home"></div> );
-}
- 
+  return (
+    <div className={`${styles.bg}`}>
+      <h5>SO, YOU WANT TO TRAVEL TO</h5>
+      <h1>SPACE</h1>
+      <Article/>
+      <div>
+        <p className={styles.explore}>EXPLORE</p>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
