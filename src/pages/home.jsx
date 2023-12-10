a87543c6d3d1b0bd00af6e1dddabad2b3b6dd34a
oneilh <obidiasodumebi@mail.com>
2023-12-10 14:20:51 +0100

    created home page(mobile)

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,19 @@
 import Article from "../components/Article/Article";
 import styles from "./home.module.css";
+
 const Home = () => {
+  const content =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam zquas ad delectus pariatur libero eveniet fugit doloremque architecto veniam officia obcaecati voluptatibus, corrupti non cum quis beatae molestiae corporis ipsa.";
+
   return (
-    <div className={`${styles.bg}`}>
-      <h5>SO, YOU WANT TO TRAVEL TO</h5>
-      <h1>SPACE</h1>
-      <Article/>
-      <div>
-        <p className={styles.explore}>EXPLORE</p>
+    <div className={`container ${styles.home}`}>
+      <div className={styles.sub__cont_1}>
+        <h5>SO, YOU WANT TO TRAVEL TO</h5>
+        <h1>SPACE</h1>
+        <Article content={content} />
+      </div>
+      <div className={styles.sub__cont_2}>
+        <div className={styles.explore}>EXPLORE</div>
       </div>
     </div>
   );
