f6968af87f3187aaccf9c688e2264ad2c0b25ef6
oneilh <obidiasodumebi@mail.com>
2023-12-10 16:24:08 +0100

    update in config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/SPACE-TOURISM/",
   plugins: [react()],
-  base: "/SPACE-TOURISM",
 })
